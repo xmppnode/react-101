5d0476849d679498201f08808ca0ac5bafdff189
xmppnode <xmpp.node@gmail.com>
2019-02-23 07:13:09 +0000

    React version
    16.8.0 ready with Hooks

--- src/index.js
+++ src/index.js
@@ -6,23 +6,19 @@ import "./styles.css";
 function App() {
   //const element = <h1>Hello, CodeSandbox</h1>;
   function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
+    return user.firstName + " " + user.lastName;
   }
 
   const user = {
-    firstName: 'Harper',
-    lastName: 'Perez'
+    firstName: "Harper",
+    lastName: "Perez"
   };
 
-  const element = (
-    <h1>
-      Hello, {formatName(user)}!
-  </h1>
-  );
+  const element = <h1>Hello, {formatName(user)}!</h1>;
   return (
     <div className="App">
       {element}
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Currently using React {React.version}</h2>
     </div>
   );
 }
